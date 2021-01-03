75db9f5798b9a66949f362b8f203c07327fcaf66
Chris Frank <chris@cfrank.org>
2021-01-02 22:07:03 -0800

    send back "hello world" to new client connections
    Signed-off-by: Chris Frank <chris@cfrank.org>

--- server.c
+++ server.c
@@ -13,6 +13,8 @@
 #define DEFAULT_PORT "4986"
 #define MAX_BACKLOG 128
 #define MAX_EVENTS 1024
+#define READ_SIZE 1000
+#define MESSAGE "Hello world"
 
 static bool make_fd_non_blocking(int fd)
 {
@@ -31,21 +33,10 @@ static bool make_fd_non_blocking(int fd)
         return true;
 }
 
-/**
- * The goal of this small program is to accept an incoming connection
- * and return the string "Hello world" back to the peer
- */
-int main(int argc, const char **argv)
+static struct addrinfo *get_addrinfo(const char *port)
 {
-        const char *port = DEFAULT_PORT;
-
-        if (argc == 2) {
-                port = argv[1];
-        }
-
-        // Set up address
+        struct addrinfo *res = NULL;
         struct addrinfo hints;
-        struct addrinfo *addrs = NULL;
 
         memset(&hints, 0, sizeof(struct addrinfo));
 
@@ -54,22 +45,21 @@ int main(int argc, const char **argv)
         hints.ai_flags = AI_PASSIVE;
         hints.ai_protocol = 0;
 
-        if (getaddrinfo(HOST, port, &hints, &addrs) != 0) {
+        if (getaddrinfo(HOST, port, &hints, &res) != 0) {
                 puts("Failed to call getaddrinfo");
 
-                return EXIT_FAILURE;
+                return NULL;
         }
 
-        if (addrs == NULL) {
-                puts("Invalid addrs result");
+        return res;
+}
 
-                return EXIT_FAILURE;
-        }
-
+static int get_listener(const struct addrinfo *addrinfo)
+{
         int fd = -1;
-
         const struct addrinfo *addr = NULL;
-        for (addr = addrs; addr != NULL; addr = addr->ai_next) {
+
+        for (addr = addrinfo; addr != NULL; addr = addr->ai_next) {
                 fd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
 
                 if (fd < 0) {
@@ -81,35 +71,49 @@ int main(int argc, const char **argv)
 
                         close(fd);
 
-                        freeaddrinfo(addrs);
-
-                        return EXIT_FAILURE;
+                        return -1;
                 }
 
-                if (listen(fd, MAX_BACKLOG) != 0) {
-                        close(fd);
-
-                        freeaddrinfo(addrs);
-
+                if (listen(fd, MAX_BACKLOG) < 0) {
                         puts("Failed to listen to fd");
 
-                        return EXIT_FAILURE;
+                        close(fd);
+
+                        return -1;
                 }
 
                 break;
         }
 
-        freeaddrinfo(addrs);
+        return fd;
+}
 
-        if (fd < 0) {
-                puts("Invalid FD");
+/**
+ * Accept a connection and return "Hello world"
+ */
+int main(int argc, const char **argv)
+{
+        const char *port = DEFAULT_PORT;
 
+        if (argc == 2) {
+                port = argv[1];
+        }
+
+        struct addrinfo *addrinfo = get_addrinfo(port);
+
+        if (addrinfo == NULL) {
                 return EXIT_FAILURE;
         }
 
-        if (!make_fd_non_blocking(fd)) {
-                freeaddrinfo(addrs);
+        int listen_fd = get_listener(addrinfo);
 
+        freeaddrinfo(addrinfo);
+
+        if (listen_fd == -1) {
+                return EXIT_FAILURE;
+        }
+
+        if (!make_fd_non_blocking(listen_fd)) {
                 return EXIT_FAILURE;
         }
 
@@ -125,12 +129,12 @@ int main(int argc, const char **argv)
         struct epoll_event ev = {};
 
         ev.events = EPOLLIN;
-        ev.data.fd = fd;
+        ev.data.fd = listen_fd;
 
-        if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &ev) == -1) {
-                puts("Failed to add fd to epoll");
+        if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listen_fd, &ev) == -1) {
+                puts("Failed to add listen_fd to epoll");
 
-                return EXIT_FAILURE;
+                goto handle_error;
         }
 
         struct epoll_event events[MAX_EVENTS];
@@ -140,19 +144,88 @@ int main(int argc, const char **argv)
                 ready_fds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
 
                 if (ready_fds == -1) {
-                        puts("epoll_wait failed");
-
-                        return EXIT_FAILURE;
+                        goto handle_error;
                 }
 
                 for (int i = 0; i < ready_fds; ++i) {
-                        if (events[i].data.fd == fd) {
-                                printf("Got %d ready fds", ready_fds);
-                        }
+                        if (events[i].events & EPOLLERR) {
+                                puts("epoll error");
 
-                        return EXIT_SUCCESS;
+                                goto handle_error;
+                        }
+                        if (events[i].data.fd == listen_fd) {
+                                // Handle new peer
+                                struct sockaddr_in peer_addr;
+                                socklen_t peer_addr_len = sizeof(peer_addr);
+                                int new_peer_fd = accept(
+                                        listen_fd, (struct sockaddr *)&peer_addr, &peer_addr_len);
+
+                                if (new_peer_fd < 0) {
+                                        puts("Failed to accept");
+
+                                        goto handle_error;
+                                }
+
+                                if (!make_fd_non_blocking(new_peer_fd)) {
+                                        puts("Failed to make peer fd non blocking");
+
+                                        close(new_peer_fd);
+
+                                        goto handle_error;
+                                }
+
+                                if (new_peer_fd >= MAX_EVENTS) {
+                                        puts("Too many events");
+
+                                        close(new_peer_fd);
+
+                                        goto handle_error;
+                                }
+
+                                struct epoll_event new_peer_event = {0};
+                                new_peer_event.data.fd = new_peer_fd;
+                                // we will only be writing to this fd
+                                new_peer_event.events |= EPOLLOUT;
+
+                                if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, new_peer_fd, &new_peer_event)
+                                    < 0) {
+                                        puts("Failed to add new peer fd to epoll");
+
+                                        close(new_peer_fd);
+
+                                        goto handle_error;
+                                }
+                        } else if (events[i].events & EPOLLOUT) {
+                                // Write to this FD
+                                int message_len = sizeof(MESSAGE);
+                                int sent_cnt = send(events[i].data.fd, MESSAGE, message_len, 0);
+
+                                if (sent_cnt < 0) {
+                                        puts("Failed to send message");
+
+                                        goto handle_error;
+                                }
+
+                                puts("disconnecting");
+
+                                if (epoll_ctl(epoll_fd, EPOLL_CTL_DEL, events[i].data.fd, NULL)
+                                    < 0) {
+                                        puts("Failed to delete fd from epoll");
+
+                                        close(events[i].data.fd);
+
+                                        goto handle_error;
+                                }
+                        }
                 }
         }
 
         return EXIT_SUCCESS;
+
+handle_error:
+        if (close(epoll_fd) != 0) {
+                puts("Failed to close epoll fd");
+        }
+
+        return EXIT_FAILURE;
 }
